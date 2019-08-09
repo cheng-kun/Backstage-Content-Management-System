e147e02495e20364dfd81dd8d9289c59baf2de4e
cheng-kun <chengkunche@yahoo.com>
2019-08-08 23:03:27 -0400

    Initial commit

---
+++ src/main/java/com/company/backstagecontentmanagementsystem/BackstageContentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.backstagecontentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageContentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackstageContentManagementSystemApplication.class, args);
+    }
+
+}
