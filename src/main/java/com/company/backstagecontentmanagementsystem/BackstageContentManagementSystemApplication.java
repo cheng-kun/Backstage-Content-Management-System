d40c790c1ec5afa126a4e1e5c238514a76d3e2d2
cheng-kun <chengkunche@yahoo.com>
2019-08-10 13:15:43 -0400

    short commit

--- src/main/java/com/company/backstagecontentmanagementsystem/BackstageContentManagementSystemApplication.java
+++ src/main/java/com/company/backstagecontentmanagementsystem/BackstageContentManagementSystemApplication.java
@@ -2,12 +2,19 @@ package com.company.backstagecontentmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackstageContentManagementSystemApplication {
+public class BackstageContentManagementSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BackstageContentManagementSystemApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BackstageContentManagementSystemApplication.class);
     }
 
 }
