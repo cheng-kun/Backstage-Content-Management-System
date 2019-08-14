e163e1a3df908a1dd8d5deda2c7a88bdc308757d
cheng-kun <chengkunche@yahoo.com>
2019-08-13 20:42:27 -0400

    update service

--- src/main/java/com/company/backstagecontentmanagementsystem/service/StoreService.java
+++ src/main/java/com/company/backstagecontentmanagementsystem/service/StoreService.java
@@ -21,9 +21,9 @@ public class StoreService {
 
     public boolean createStore(Store store) {
         try {
-            storeMapper.createStore(store.getName(), store.getLogo(), store.getAddress(), store.getCategory(), store.getDescription(),
+            int affected = storeMapper.createStore(store.getName(), store.getLogo(), store.getAddress(), store.getCategory(), store.getDescription(),
                     store.getPhone(), store.getAvgPrice(), store.getSaleFrom(), store.getSaleTo(), store.getUser().getUserId());
-            return true;
+            return affected > 0;
         } catch (SQLException e) {
             logger.error("create store failed", e);
         }
@@ -32,9 +32,9 @@ public class StoreService {
 
     public boolean updateStore(Store store) {
         try {
-            storeMapper.updateStore(store.getName(), store.getLogo(), store.getAddress(), store.getCategory(), store.getDescription(),
+           int affected = storeMapper.updateStore(store.getName(), store.getLogo(), store.getAddress(), store.getCategory(), store.getDescription(),
                     store.getPhone(), store.getAvgPrice(), store.getSaleFrom(), store.getSaleTo(), store.getUser().getUserId());
-            return true;
+            return affected > 0;
         } catch (SQLException e) {
             logger.error("update store failed", e);
         }
