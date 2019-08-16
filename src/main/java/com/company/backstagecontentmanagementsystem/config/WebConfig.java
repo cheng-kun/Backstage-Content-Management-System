700be28a8ee2d07f7207b1b6c5f90e6d0803be4e
cheng-kun <chengkunche@yahoo.com>
2019-08-15 20:47:26 -0400

    cache config

--- src/main/java/com/company/backstagecontentmanagementsystem/config/WebConfig.java
+++ src/main/java/com/company/backstagecontentmanagementsystem/config/WebConfig.java
@@ -1,14 +1,26 @@
 package com.company.backstagecontentmanagementsystem.config;
 
 import com.company.backstagecontentmanagementsystem.handler.CustomInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry interceptorRegistry){
         super.addInterceptors(interceptorRegistry);
-        interceptorRegistry.addInterceptor(new CustomInterceptor()).addPathPatterns("/**").excludePathPatterns("login","register");
+        interceptorRegistry.addInterceptor(new CustomInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/login", "/user/register", "/hello");
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/register.html").setViewName("register");
+        registry.addViewController("/goods_main.html").setViewName("goods_main");
+    }
+
 }
