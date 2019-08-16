cb4a3e1be81720f84fb7512ba9b17f42487d0b20
cheng-kun <chengkunche@yahoo.com>
2019-08-15 20:49:04 -0400

    implements WebApplicationInitializer

--- src/main/java/com/company/backstagecontentmanagementsystem/BackstageContentManagementSystemApplication.java
+++ src/main/java/com/company/backstagecontentmanagementsystem/BackstageContentManagementSystemApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-@EnableTransactionManagement
-public class BackstageContentManagementSystemApplication extends SpringBootServletInitializer {
+public class BackstageContentManagementSystemApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BackstageContentManagementSystemApplication.class, args);
