d2c36a7aef092667ae4a1aee8fff7a04aebc16ab
cheng-kun <chengkunche@yahoo.com>
2019-08-15 20:24:11 -0400

    cache annotation

--- src/main/java/com/company/backstagecontentmanagementsystem/service/GoodsService.java
+++ src/main/java/com/company/backstagecontentmanagementsystem/service/GoodsService.java
@@ -5,6 +5,8 @@ import com.company.backstagecontentmanagementsystem.domain.Goods;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
@@ -20,6 +22,7 @@ public class GoodsService {
         this.goodsMapper = goodsMapper;
     }
 
+    @CacheEvict(value = "deleteGoods", key = "'goodsPage'")
     public boolean createGoods(Goods goods, int userId) {
         try {
             int key = goodsMapper.createGoods(goods.getGname(), goods.getPicture(), goods.getSpecification(), goods.getPrice(), goods.getStock(),
@@ -31,6 +34,7 @@ public class GoodsService {
         return false;
     }
 
+    @CacheEvict(value = "deleteGoods", key = "'goodsPage'")
     public boolean updateGoods(Goods goods) {
         try {
             int affected = goodsMapper.updateGoods(goods.getGname(), goods.getPicture(), goods.getSpecification(), goods.getPrice(), goods.getStock(),
@@ -42,6 +46,7 @@ public class GoodsService {
         return false;
     }
 
+    @CacheEvict(value = "deleteGoods", key = "'goodsPage'")
     public boolean deleteGoods(int goodsId) {
         try {
             int affected = goodsMapper.deleteGoods(goodsId);
@@ -52,6 +57,7 @@ public class GoodsService {
         return false;
     }
 
+    @Cacheable(value = "goodsByPage", key = "'goodsPage'")
     public List<Goods> queryGoodsByPage(int pageIndex, int pageSize, int userId) {
         if (--pageIndex < 0) {
             pageIndex = 0;
