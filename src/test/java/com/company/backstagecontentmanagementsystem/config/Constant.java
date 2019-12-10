854eb22fe619997552a3024781e2784a8020000a
cheng-kun <chengkunche@yahoo.com>
2019-12-09 19:58:51 -0500

    config constant class

---
+++ src/test/java/com/company/backstagecontentmanagementsystem/config/Constant.java
@@ -0,0 +1,6 @@
+package com.company.backstagecontentmanagementsystem.config;
+
+public class Constant {
+    public static final String USER_TOKEN = "USER_TOKEN";
+    public static final String NULL_TOKEN = "-1";
+}
