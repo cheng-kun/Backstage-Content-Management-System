474eef9a3437c0275b9a070cc3f5b6ab2a8ca8f3
cheng-kun <chengkunche@yahoo.com>
2019-12-11 21:29:46 -0500

    reverted

--- src/test/java/com/company/backstagecontentmanagementsystem/config/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.company.backstagecontentmanagementsystem.config;
-
-public class Constant {
-    public static final String USER_TOKEN = "USER_TOKEN";
-    public static final String NULL_TOKEN = "-1";
-}
